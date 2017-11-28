632efa8d469b4a3040229fd363d98a36948ddcef
Chris Dahms <cdahms@nascent.com>
2017-11-28 14:52:24 -0500

    more name changes

--- Main.cpp
+++ Main.cpp
@@ -33,54 +33,65 @@ int main() {
 
     // sharpen the image
 
+    
     // Create a kernel that we will use for accuting/sharpening our image
     // An approximation of second derivative, a quite strong kernel, do the laplacian filtering as it is
     // well, we need to convert everything in something more deeper then CV_8U because the kernel has some negative values,
     // and we can expect in general to have a Laplacian image with negative values BUT a 8bits unsigned int (the one we are working with)
     // can contain values from 0 to 255 so the possible negative number will be truncated
-    cv::Mat kernel = (cv::Mat_<float>(3, 3) <<  1, 1, 1,
-                                                1, -8, 1,
-                                                1, 1, 1);
+    cv::Mat matKernel = (cv::Mat_<float>(3, 3) << 1, 1, 1,
+                                                  1, -8, 1,
+                                                  1, 1, 1);
 
+    // ToDo: is imgLaplacian ever used ??
     cv::Mat imgLaplacian;
     cv::Mat imgSharp = imgOriginal; // copy source image to another temporary one
-    filter2D(imgSharp, imgLaplacian, CV_32F, kernel);
+    cv::filter2D(imgSharp, imgLaplacian, CV_32F, matKernel);
     imgOriginal.convertTo(imgSharp, CV_32F);
-    cv::Mat imgResult = imgSharp - imgLaplacian;
+    cv::Mat imgSharpened = imgSharp - imgLaplacian;
     // convert back to 8bits gray scale
-    imgResult.convertTo(imgResult, CV_8UC3);
+    imgSharpened.convertTo(imgSharpened, CV_8UC3);
     imgLaplacian.convertTo(imgLaplacian, CV_8UC3);
     // imshow( "Laplace Filtered Image", imgLaplacian );
-    imshow("New Sharped Image", imgResult);
-    imgOriginal = imgResult; // copy back
+    imshow("3 - New Sharped Image", imgSharpened);
+    imgOriginal = imgSharpened; // copy back
+    
                      // Create binary image from source image
-    cv::Mat bw;
-    cvtColor(imgOriginal, bw, CV_BGR2GRAY);
-    threshold(bw, bw, 40, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
-    imshow("Binary Image", bw);
+
+
+    // convert to grayscale
+    cv::Mat imgGrayscale;
+    cvtColor(imgOriginal, imgGrayscale, CV_BGR2GRAY);
+    cv::imshow("4 - imgGrayscale", imgGrayscale);
+
+    // threshold
+    cv::Mat imgThresh;
+    cv::threshold(imgGrayscale, imgThresh, 40.0, 255.0, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    cv::imshow("5 - imgThresh", imgThresh);
+
     // Perform the distance transform algorithm
-    cv::Mat dist;
-    distanceTransform(bw, dist, CV_DIST_L2, 3);
-    // Normalize the distance image for range = {0.0, 1.0}
-    // so we can visualize and threshold it
-    normalize(dist, dist, 0, 1., cv::NORM_MINMAX);
-    imshow("Distance Transform Image", dist);
-    // Threshold to obtain the peaks
-    // This will be the markers for the foreground objects
-    threshold(dist, dist, .4, 1., CV_THRESH_BINARY);
-    // Dilate a bit the dist image
+    cv::Mat imgDistTransResult;
+    cv::distanceTransform(imgThresh, imgDistTransResult, CV_DIST_L2, 3);
+    // normalize the distance image for range = {0.0, 1.0} so we can visualize and threshold it
+    normalize(imgDistTransResult, imgDistTransResult, 0, 1., cv::NORM_MINMAX);
+    cv::imshow("Distance Transform Image", imgDistTransResult);
+
+    // threshold to obtain the peaks, these will be the markers for the foreground objects
+    cv::threshold(imgDistTransResult, imgDistTransResult, 0.4, 1.0, CV_THRESH_BINARY);
+
+    // dilate
     cv::Mat kernel1 = cv::Mat::ones(3, 3, CV_8UC1);
-    dilate(dist, dist, kernel1);
-    imshow("Peaks", dist);
-    // Create the CV_8U version of the distance image
-    // It is needed for findContours()
+    cv::dilate(imgDistTransResult, imgDistTransResult, kernel1);
+    cv::imshow("Peaks", imgDistTransResult);
+
+    // create the CV_8U version of the distance image, needed for findContours()
     cv::Mat dist_8u;
-    dist.convertTo(dist_8u, CV_8U);
+    imgDistTransResult.convertTo(dist_8u, CV_8U);
     // Find total markers
-    std::vector<std::vector<cv::Point> > contours;
-    findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
     // Create the marker image for the watershed algorithm
-    cv::Mat markers = cv::Mat::zeros(dist.size(), CV_32SC1);
+    cv::Mat markers = cv::Mat::zeros(imgDistTransResult.size(), CV_32SC1);
     // Draw the foreground markers
     for (size_t i = 0; i < contours.size(); i++)
         drawContours(markers, contours, static_cast<int>(i), cv::Scalar::all(static_cast<int>(i) + 1), -1);
